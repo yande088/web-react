bc9f590cdd6279360cf500082eff99699ad4fb0d
yande088 <harold.rojas088@gmail.com>
2025-01-10 11:13:11 -0500

    elimando datepickers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from 'antd';
-import type { DatePickerProps } from 'antd';
-import { DatePicker, Space } from 'antd';
-
-const onChange: DatePickerProps['onChange'] = (date, dateString) => {
-  console.log(date, dateString);
-};
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,14 +9,6 @@ function App() {
     <>
       <p>Harolito { count } </p>
       <Button onClick={() => setCount(count +1 )} type="primary">Button</Button>
-      
-      <Space direction="vertical">
-        <DatePicker onChange={onChange} />
-        <DatePicker onChange={onChange} picker="week" />
-        <DatePicker onChange={onChange} picker="month" />
-        <DatePicker onChange={onChange} picker="quarter" />
-        <DatePicker onChange={onChange} picker="year" />
-      </Space>
     </>
   )
 }
